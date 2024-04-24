0663400e1e1c40293dcfbaf0f72012a8eb17b371
Matt McCoy <mattcmccoy01@gmail.com>
2024-04-24 14:19:00 -0400

    (style): font changes

--- src/app/(home)/landing.tsx
+++ src/app/(home)/landing.tsx
@@ -1,16 +1,16 @@
 export function Landing() {
   return (
-    <div className="w-[80vw] border-b border-yellow-300 pb-10 text-center">
-      <div className="font-titillium text-4xl font-thin tracking-[2vw] lg:text-8xl">
-        Matthew McCoy
+    <div className="w-fit border-b border-yellow-300 pb-10 text-center">
+      <div className="font-titillium text-4xl font-thin tracking-[1.5vw] lg:text-8xl">
+        Matthew C. McCoy
       </div>
-      <div className="font-titillium text-6xl font-extrabold lg:text-8xl">
+      <div className="font-titillium text-6xl font-semibold lg:text-8xl">
         Developer.
       </div>
-      <div className="font-titillium text-6xl font-extrabold  lg:text-8xl">
+      <div className="font-titillium text-6xl font-semibold  lg:text-8xl">
         Photographer.
       </div>
-      <div className="mt-2 font-titillium text-6xl font-extrabold  lg:text-8xl">
+      <div className="mt-2 font-titillium text-6xl font-semibold  lg:text-8xl">
         Hiker.
       </div>
     </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Experience } from "./(home)/experience";
 
 export default async function Home() {
   return (
-    <main className="animated-background flex min-h-screen flex-col items-center justify-center bg-gradient-to-r from-[#15162c] via-[#15162c] to-[#342544] font-titillium text-white">
+    <main className="animated-background flex min-h-screen flex-col items-center justify-center bg-gradient-to-r from-[#15162c] via-[#15162c] to-[#140f19] font-titillium text-white">
       <div className="flex h-[100vh] w-full flex-col items-center justify-center">
         <Landing />
       </div>
