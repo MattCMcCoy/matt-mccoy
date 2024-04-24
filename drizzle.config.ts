9337d85a3f619b1740369e9b55dcfe43087180e6
Matt McCoy <mattcmccoy01@gmail.com>
2024-04-24 09:37:44 -0400

    init t3 stack

---
+++ drizzle.config.ts
@@ -0,0 +1,14 @@
+import { type Config } from "drizzle-kit";
+
+import { env } from "~/env";
+
+export default {
+  schema: "./src/server/db/schema.ts",
+  out: "./src/server/db/migrations",
+  driver: "turso",
+  dbCredentials: {
+    url: env.TURSO_DATABASE_URL,
+    authToken: env.TURSO_AUTH_TOKEN,
+  },
+  tablesFilter: ["matt-mccoy_*"],
+} satisfies Config;
