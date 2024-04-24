483bdd21ad1c1daca811977bc399ac7f279251c8
Matt McCoy <mattcmccoy01@gmail.com>
2024-04-24 13:11:57 -0400

    (feat): experience cards

---
+++ src/app/(home)/landing.tsx
@@ -0,0 +1,18 @@
+export function Landing() {
+  return (
+    <div className="w-[80vw] border-b border-yellow-300 pb-10 text-center">
+      <div className="font-titillium text-4xl font-thin tracking-[2vw] lg:text-8xl">
+        Matthew McCoy
+      </div>
+      <div className="font-titillium text-6xl font-extrabold lg:text-8xl">
+        Developer.
+      </div>
+      <div className="font-titillium text-6xl font-extrabold  lg:text-8xl">
+        Photographer.
+      </div>
+      <div className="mt-2 font-titillium text-6xl font-extrabold  lg:text-8xl">
+        Hiker.
+      </div>
+    </div>
+  );
+}
