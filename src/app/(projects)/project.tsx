625f89611ca115f4368647e4b42e1c7e8deb80c2
Matt McCoy <mattcmccoy01@gmail.com>
2024-04-25 19:58:23 -0400

    feat: start to project section on main page

--- src/app/(projects)/project.tsx
+++ src/app/(projects)/project.tsx
@@ -1,12 +1,21 @@
 "use client";
 
+import { clsx } from "clsx";
+import Image from "next/image";
 import { api } from "~/trpc/react";
+
+import Website from "../assets/website.gif";
+import CareWallet from "../assets/carewallet.gif";
+import Portfolio from "../assets/portfolio1.gif";
+import CoveyTown from "../assets/coveytown.gif";
+import StreamingAvailability from "../assets/streamingavailability.gif";
+import AgarIO from "../assets/agario.gif";
 
 export function Project() {
   const { data: project } = api.project.getAll.useQuery();
 
   return (
-    <div className="mx-10 mr-auto mt-20">
+    <div className="mx-10 mt-20 lg:mr-auto">
       <div className="border-portfolio-indigo_dye-700  mb-5 w-fit border-b font-titillium text-5xl font-extrabold">
         Projects.
       </div>
@@ -15,13 +24,30 @@ export function Project() {
           return (
             <div
               key={project.id}
-              className="flex w-full flex-col gap-2 text-white"
+              className={clsx(
+                "mt-20 flex w-full flex-col items-center justify-center gap-2 text-white lg:flex-row",
+              )}
             >
-              <div className="font-titillium text-3xl font-light">
-                {project.title}
+              <div className="mr-10 flex flex-col">
+                <div className="font-titillium text-3xl font-light">
+                  {project.title}
+                </div>
+                <div className="w-[600px] font-titillium text-base font-light">
+                  {project.description}
+                </div>
               </div>
-              <div className="font-titillium text-base font-light">
-                {project.description}
+              <div className="mx-auto mr-auto h-fit">
+                <Image
+                  src={getGif(project.title).image}
+                  alt=""
+                  className={clsx(
+                    "border-none fill-none",
+                    project.mobile ? "" : "h-[20vh]",
+                  )}
+                  unoptimized
+                  width={project.mobile ? 175 : 450}
+                  height={project.mobile ? 175 : 200}
+                />
               </div>
             </div>
           );
@@ -30,3 +56,37 @@ export function Project() {
     </div>
   );
 }
+
+const getGif = (name: string) => {
+  switch (name) {
+    case "Generate's Website":
+      return {
+        image: Website,
+      };
+    case "Care-Wallet":
+      return {
+        image: CareWallet,
+      };
+    case "Portfolio":
+      return {
+        image: Portfolio,
+      };
+    case "Covey.Town - Checkers":
+      return {
+        image: CoveyTown,
+      };
+    case "Streaming Availability":
+      return {
+        image: StreamingAvailability,
+      };
+    case "Agar.io Clone":
+      return {
+        image: AgarIO,
+      };
+    default:
+      return {
+        image:
+          "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExbjZyanplNzBsZDhmMWJjN2N3OXhpOXFwamhqd3VkcDVieWEwY3QyMCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/TLeLKUdIc1tvAxb7ab/giphy.gif",
+      };
+  }
+};
--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -59,19 +59,22 @@ const projects = [
     startDate: "December 20, 2023",
     endDate: "April 20, 2024",
     image: "generate",
+    mobile: true,
     githuburl: "",
-    description: "",
+    description:
+      "A mobile app meant to simplify the care of loved ones with Alzheimer's and Dementia. Built on React Native, TypeScript, and Golang. The app allows for Caretakers to track daily activities, medication, and appointments for loved ones easily with a centralized calendar and task management system within the application.",
   },
   {
     id: 2,
-    title: "Generate Website",
+    title: "Generate's Website",
     position: "Software Engineer",
     organization: "Generate",
     startDate: "September 10, 2023",
     endDate: "December 20, 2023",
     image: "generate",
     githuburl: "",
-    description: "",
+    description:
+      "A website built for Generate Product Development at Northeastern University. The website was built using React and JavaScript. The website is meant to showcase the projects that the team has worked on and the team members. The website is also meant to be a resource for potential clients to learn more about the team and the projects that the team has worked on.",
   },
   {
     id: 3,
@@ -82,29 +85,32 @@ const projects = [
     endDate: "April 20, 2024",
     image: "",
     githuburl: "",
-    description: "",
+    description:
+      "A site developed to track the availability of streaming services for movies. Built using Next.js, TailwindCSS, and TypeScript. The site allows users to search for a movie and see which streaming services it is available on. The site also allows users to see what is available on a specific streaming service. The site uses the TMDB API to get the availability of movies.",
   },
   {
     id: 4,
-    title: "Portfolio V1",
+    title: "Portfolio",
     position: "",
     organization: "",
     startDate: "July 10, 2022",
     endDate: "December 20, 2022",
     image: "",
     githuburl: "",
-    description: "",
+    description:
+      "Developed during my very first Co-op. Built using React JS and TypeScript. The site allowed me to showcase my work and experience while I learned more about web development.",
   },
   {
     id: 5,
-    title: "Portfolio V2",
+    title: "Portfolio Revamped",
     position: "",
     organization: "",
     startDate: "April 24, 2024",
     endDate: "",
     image: "",
     githuburl: "",
-    description: "",
+    description:
+      "The site you are on right now and a refresh of my original portfolio. Meant to showcase my work and experience in a more modern and clean way. Built using Next.js, TailwindCSS, and TypeScript.",
   },
   {
     id: 6,
@@ -115,18 +121,20 @@ const projects = [
     endDate: "April 24, 2023",
     image: "",
     githuburl: "",
-    description: "",
+    description:
+      "A game of checkers built for the Covey.Town platform. Built using React and TypeScript. The game allows players to play checkers with other players in a Covey.Town room. The game was built to learn more about adding to larger code bases, game development, and React.",
   },
   {
     id: 7,
-    title: "Agar.IO Clone",
+    title: "Agar.io Clone",
     position: "",
     organization: "",
-    startDate: "September 20, 2021",
-    endDate: "November 20, 2021",
+    startDate: "February 20, 2021",
+    endDate: "May 20, 2021",
     image: "",
-    githuburl: "",
-    description: "",
+    githuburl: "https://github.com/MattCMcCoy/AgarIOGame",
+    description:
+      "A clone of the popular game Agar.io and one of my very first projects. Built using Java. The game allows players to move around the screen and eat other blobs to grow in size. The game was built to learn more about game development and Object Oriented Programming.",
   },
 ];
 
@@ -142,10 +150,10 @@ const education = [
 ];
 
 const landing = {
-  name: "Matt McCoy",
-  tagline: "Photography. Hiking. Kayaking. Coffee. Developer.",
+  name: "Matthew C McCoy",
+  tagline: "Photographer. Hiker. Kayaker. Coffee Lover. Developer.",
   about:
-    "I am a software engineer with a passion for photography, hiking, kayaking, and coffee. I have experience across the stack and love finding new technologies to play around with. I am a graduate of Northeastern University in Boston MA with a bachelor's degree in Computer Science.",
+    "I am a software engineer with a passion for photography, hiking, kayaking, and coffee. I have experience across the stack and love finding new technologies to play around with. I am a graduate of Northeastern University in Boston MA with a bachelor's degree in Computer Science. I have experience in software engineering, test automation, and teaching. I am always looking for new opportunities to learn and grow.",
 };
 
 export const StaticJSONDatabase = {
